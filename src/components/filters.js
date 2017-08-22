d545716f090215b75e65ee776022f50876f6cf1a
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2017-08-22 10:39:25 +0530

    Added on click function to searchbox component. Yet to make the component truly reusable

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class SearchBox extends React.Component {
+
+  handleEnterKeyPress = (e) => {
+    if(e.key == 'Enter'){
+      this.sendQuery()
+    }
+  }
+
+  sendQuery(){
+    alert("Fetching books...")
+  }
+
   render() {
     return (
       <div>
-        <input type="text" />
-        <button type="submit">Search</button>
+        <input type="text" onKeyPress={ this.handleEnterKeyPress.bind(this) } />
+        <button type="submit" onClick={ this.sendQuery.bind(this) } >Search</button>
       </div>
     )
   }
